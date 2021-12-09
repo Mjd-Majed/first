f2dc4673621842c88aec72a9da364bb76f8935af
Mjd-Majed <68184357+Mjd-Majed@users.noreply.github.com>
2021-12-10 00:13:53 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
-basic.showLeds(`
-    # . . . .
-    . # . . .
-    . . # . .
-    . . . # .
-    . . . . #
-    `)
 basic.forever(function () {
     basic.showNumber(0)
     basic.showNumber(7)
+    basic.showString("M")
+    basic.showIcon(IconNames.TShirt)
 })
